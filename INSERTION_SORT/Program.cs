2410e85b5835e1e12f982d3fd16d0f1b652f6e36
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-02 22:58:05 +0530

    added insertion sort

---
+++ INSERTION_SORT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {     //Reads in strings and prints them in sorted order using insertion sort.
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 23, 9, 85, 12, 99 };
+            int n = 5, i, j, val, flag;
+            Console.WriteLine("Insertion Sort");
+            Console.WriteLine("Intial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+            Console.Write("\nSorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
