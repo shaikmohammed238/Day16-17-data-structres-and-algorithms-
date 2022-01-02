33cc188d4c97fa79574257a7445c008ace317ca9
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-02 23:21:51 +0530

    added two string

---
+++ TwoStringAreEqual/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TwoStringAreEqual
+{  // Check if the arrays returned by two string functions are equal.
+    class Program
+    {
+        public static bool checkEquality(String[] s1, String[] s2)
+        {
+            if (s1 == s2)
+            {
+                return true;
+            }
+            if (s1 == null || s2 == null)
+            {
+                return false;
+            }
+            int n = s1.Length;
+            if (n != s2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (!s1[i].Equals(s2[i]))
+                {
+                  return false;
+                }
+            }
+                 return true;
+        }
+        class program
+        {
+            static void Main(String[] args)
+            {
+                String[] s1 = { "A", "B", "C" };
+                String[] s2 = { "A", "B", "C" };
+                if (checkEquality(s1, s2))
+                {
+                    Console.WriteLine("Both arrays are equal");
+                }
+                else
+                {
+                    Console.WriteLine("Both arrays are not equal");
+                }
+            }
+        }
+    }
+}
