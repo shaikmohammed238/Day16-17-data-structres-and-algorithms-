76541f5d78fcf78073bdb9db3bdaaff21f2d6a08
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-02 22:48:17 +0530

    added 1stproblem

---
+++ Anagram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Anagram
+{
+    class Program
+    {    //-> Take 2 Strings as Input such abcd and dcba and Check for Anagrams
+        static void Main(string[] args)
+        {
+            string str1 = "heater";
+            string str2 = "reheat";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Anagram-------{0}----{1}", val1, val2);
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+
+        }
+    }
+}
